cc8755aaa4215a6876e97a8e0f48df5b394f8953
Dimitar Apostolovski <8737118+themetar@users.noreply.github.com>
2022-02-07 22:16:29 +0100

    Use integer codes to mark phrases

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,17 +6,29 @@ class Computer
     @guesses = []
   end
 
-  def next_guess()
+  def next_guess
     # guess randomly
-    phrase = loop do
-      guess = Array.new(Mastermind::PHRASE_SIZE) { 1 + rand(6) }
-      break guess unless @guesses.include?(phrase)
+    code = loop do
+      guess = rand(6 ** Mastermind::PHRASE_SIZE)
+      break guess unless @guesses.include?(guess)
     end
 
-    @guesses << phrase # remember so it won't repeat
+    @guesses << code # remember so it won't repeat
 
     sleep 0.75 # dramatic pause
 
-    phrase
+    convert(code)
+  end
+
+  private
+
+  def convert(int_code)
+    out = []
+    until out.length == Mastermind::PHRASE_SIZE
+      digit = int_code % 6
+      out.unshift(digit + 1)
+      int_code = (int_code - digit) / 6
+    end
+    out
   end
 end
---
+++ spec/computer_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../lib/players/computer'
+
+describe Computer do
+ describe 'private #convert method' do
+  it 'converts 0 to 1 1 1 1' do
+    expect(Computer.new.send(:convert, 0)).to eq([1, 1, 1, 1])
+  end
+
+  it 'converts 1 to 1 1 1 2' do
+    expect(Computer.new.send(:convert, 1)).to eq([1, 1, 1, 2])
+  end
+
+  it 'converts 5 to 1 1 1 6' do
+    expect(Computer.new.send(:convert, 5)).to eq([1, 1, 1, 6])
+  end
+
+  it 'converts 6 to 1 1 2 1' do
+    expect(Computer.new.send(:convert, 6)).to eq([1, 1, 2, 1])
+  end
+
+  it 'converts 308 to 2 3 4 3' do
+    expect(Computer.new.send(:convert, 308)).to eq([2, 3, 4, 3])
+  end
+ end
+end
