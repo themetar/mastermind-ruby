1d805a3890d55a0fe076bd842c9d2e4de1eb2925
Dimitar Apostolovski <8737118+themetar@users.noreply.github.com>
2022-02-09 21:43:52 +0100

    Modify code validation message in Human#next_guess

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -9,7 +9,7 @@ class Human
     # prompt the user for a guess
     guess = prompt.ask("Input code:") do |ans|
               ans.validate Regexp.new "^[1-6]{#{Mastermind::PHRASE_SIZE}}$"
-              ans.messages[:valid?] = "Your guess must consist of #{Mastermind::PHRASE_SIZE} digits, from 1 to 6"
+              ans.messages[:valid?] = "The code must consist of #{Mastermind::PHRASE_SIZE} digits, from 1 to 6"
             end
 
     # convert the input string to an array of digits 
